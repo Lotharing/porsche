ee6e29e62910c76397ce5aa77641602b2799648f
Lotharing <809676295@qq.com>
2017-11-24 17:26:44 +0800

    01

---
+++ indexjava.js
@@ -0,0 +1,29 @@
+	// 存放车辆配置器图片数组
+	var cars = new Array("image/car/image01.jpg","image/car/image02.jpg","image/car/image03.jpg",
+						 "image/car/image04.jpg","image/car/image05.jpg");
+	//用于车辆配置器切换
+	function playNext(){
+		var flag = cars[0];
+		for (var i = 1; i < cars.length; i++) {
+			cars[i - 1] = cars[i];
+		}
+		cars[cars.length - 1] = flag;
+		setImgSrc();
+	}
+	function playLast(){
+		var flag = cars[cars.length - 1];
+		for (var i = cars.length - 2; i >= 0; i--) {
+			cars[i + 1] = cars[i];
+		}
+		cars[0] = flag;
+		setImgSrc();
+	}
+	//设置车辆配置器图片
+	function setImgSrc(){
+		document.getElementById("cars_one").src = cars[0];
+		document.getElementById("cars_two").src = cars[1];
+		document.getElementById("cars_three").src = cars[2];
+	}
+	//****************************************轮播图
+	var slideshows = new Array("image/timer/image01.jpg","image/timer/image02.jpg","image/timer/image03.jpg",
+							   "image/timer/image04.jpg","image/timer/image05.jpg");
